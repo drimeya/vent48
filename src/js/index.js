9a4c456a7a0648aa6308d59bf5c158c4cdd69920
rbru user7 <rbruuser7@rbrus-Mac-mini.local>
2022-12-27 14:14:47 +0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
-import "./import/modules";
-import "./import/components";
+document.addEventListener("DOMContentLoaded", function() {
+
+    const searchTrigger = document.getElementById("search-trigger"), 
+        search = document.querySelector(".header__menu__search"),
+        closeTrigger = document.getElementById("close-trigger");
+
+    function toggle(arr) {
+        arr.forEach(item => {
+            item.classList.toggle("active");
+        });
+    }
+    if(searchTrigger && search && closeTrigger) {
+        searchTrigger.addEventListener("click", () => {
+            toggle([searchTrigger, search]);
+        });
+        closeTrigger.addEventListener("click", () => {
+            toggle([searchTrigger, search]);
+        });
+    }
+});
